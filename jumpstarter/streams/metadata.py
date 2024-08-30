48b6879052c91f39c2a1a325ff19b3f7a9a59f13
Nick Cao <nickcao@nichi.co>
2024-08-30 10:19:23 -0400

    Implement MetadataStream

---
+++ jumpstarter/streams/metadata.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from typing import Any, Callable, Mapping
+
+from anyio import TypedAttributeSet, typed_attribute
+from anyio.abc import AnyByteStream, ObjectStream
+
+
+class MetadataStreamAttributes(TypedAttributeSet):
+    # https://grpc.io/docs/guides/metadata/
+    metadata: dict[str, str | bytes] = typed_attribute()
+
+
+@dataclass(kw_only=True)
+class MetadataStream(ObjectStream[bytes]):
+    stream: AnyByteStream
+    metadata: dict[str, str | bytes]
+
+    async def send(self, data: bytes) -> None:
+        await self.stream.send(data)
+
+    async def receive(self) -> bytes:
+        return await self.stream.receive()
+
+    async def send_eof(self) -> None:
+        await self.stream.send_eof()
+
+    async def aclose(self):
+        await self.stream.aclose()
+
+    @property
+    def extra_attributes(self) -> Mapping[Any, Callable[[], Any]]:
+        return self.stream.extra_attributes | {MetadataStreamAttributes.metadata: lambda: self.metadata}
